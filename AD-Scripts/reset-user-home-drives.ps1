a7fc72f2fad6d77c8f2e27d2ea04b67c8fad4337
hmiller-CSI <47221551+hmiller-CSI@users.noreply.github.com>
2019-01-31 11:55:01 -0700

    Add files via upload

---
+++ AD-Scripts/reset-user-home-drives.ps1
@@ -0,0 +1,17 @@
+﻿# Reset user home drive directories - handy for server/file share cutovers.
+
+# To reset home drives en masse (assumes everyone in AD is using the same server/path):
+
+Get-ADUser -Filter * | Foreach-Object{
+
+    $sam = $_.SamAccountName
+    Set-ADuser -Identity $_ -HomeDrive "H:" -HomeDirectory "\\server\share\$sam"}
+
+
+# To reset home drives based on OU:
+
+get-aduser -filter 'Name -like "*"' -searchbase 'OU=Nested OU,OU=Nested OU,OU=Parent OU,DC=domain,DC=com' | Foreach-Object{
+
+    $sam = $_.SamAccountName
+    Set-ADuser -Identity $_ -HomeDrive "H:" -HomeDirectory "\\server\share\$sam"}
+

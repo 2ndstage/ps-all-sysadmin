ac9310b735f5e0f014e58ca6a7a6906694a222cc
2ndstage <2ndstage@gmail.com>
2019-01-31 10:14:10 -0600

    Create Find XP Systems.ps1

---
+++ AD-Scripts/Find XP Systems.ps1
@@ -0,0 +1,25 @@
+﻿$XP = Get-ADComputer -Filter {OperatingSystem -like "*XP*"} `
+    -Properties Name, DNSHostName, OperatingSystem, `
+        OperatingSystemServicePack, OperatingSystemVersion, PasswordLastSet, `
+        whenCreated, whenChanged, LastLogonTimestamp, nTSecurityDescriptor, `
+        DistinguishedName |
+    Where-Object {$_.whenChanged -gt $((Get-Date).AddDays(-90))} |
+    Select-Object Name, DNSHostName, OperatingSystem, `
+        OperatingSystemServicePack, OperatingSystemVersion, PasswordLastSet, `
+        whenCreated, whenChanged, `
+        @{name='LastLogonTimestampDT';`
+          Expression={[datetime]::FromFileTimeUTC($_.LastLogonTimestamp)}}, `
+        @{name='Owner';`
+          Expression={$_.nTSecurityDescriptor.Owner}}, `
+        DistinguishedName
+
+
+
+        ####Export to CSV
+        ##  $XP | Export-CSV .\xp.csv
+
+        ###Export to Gridview
+        ##  $XP | Out-GridView
+        
+        ###Count
+        ### ($XP | Measure-Object).Count
